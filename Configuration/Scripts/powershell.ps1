49622cecc07a75521de1a5a7804b66ded323c718
Erland Abazi <erlandabazi@protonmail.com>
2025-07-17 21:39:46 +0200

    Powershell correction

--- Configuration/Scripts/powershell.ps1
+++ Configuration/Scripts/powershell.ps1
@@ -1,7 +1,7 @@
 # This is the script that sets the tunnel to start automatically on boot:
 
 Start-Process 'C:\Program Files\WireGuard\wireguard.exe' -ArgumentList '/installtunnelservice', '<TUNNEL_NAME>.conf' -Wait -NoNewWindow -PassThru | Out-Null
-Start-Process sc.exe -ArgumentList 'config', 'WireGuardTunnel$<TUNNEL_NAME>-ATU', 'start= auto' -Wait -NoNewWindow -PassThru | Out-Null
+Start-Process sc.exe -ArgumentList 'config', 'WireGuardTunnel$<TUNNEL_NAME>', 'start= auto' -Wait -NoNewWindow -PassThru | Out-Null
 Start-Service -Name WireGuardTunnel$<TUNNEL_NAME> -ErrorAction SilentlyContinue
 
 just replace <TUNNEL_NAME> with the one you configured on the client.
